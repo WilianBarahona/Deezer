b89dd50c7c705f3531c7eae1e9738f5501123d88
PaolaIzaguirre <eviizaguirre@yahoo.com>
2017-06-14 20:03:39 -0600

    correcion de las configuracion del perfil
    configuracion_Cuenta, favorites

--- js/player.js
+++ js/player.js
@@ -17,9 +17,24 @@ function playList(){
 	this.isPlaying=false;
 	// Definicion DE ARREGLO PLAYLIST (El id se puede obtener de una DataBase)
 	// Donde esté guardado el URL del archivo, cover, informacion de artista, etc.
+	// Elementos
+	// {id:"photo",url:"path/to/song.mp3", cover: "path/to/img.jpg(png) , artist: John Doe, title: Some song"}
 	this.arraySong = [
-		{id:"photo",url:"music/photo.mp3"},//, + iteraciones // Agregar coverimage, artist, titulo
-		{id:"rend",url:"music/rend.mp3"}//, + iteraciones
+	//, + iteraciones // Agregar coverimage, artist, titulo
+		{
+			id:"photo",
+			title: "Photograph",
+			artist: "Ed Sheeran",
+			url:"music/photo.mp3",
+			cover: "img/x.jpg",
+		},
+		{
+			id:"rend",
+			title: "My Lighthouse",
+			artist: "Rend Collective",
+			url:"music/rend.mp3",
+			cover: "img/art_of_celebration.jpg",
+		}
 	];
 	this.i=0; // CONTADOR DE REPRODUCCION
 	this.initSounds = function(){
@@ -74,15 +89,44 @@ function playList(){
 		this.i--;
 		if (this.i<this.arraySong.length && this.i>=0) {
 			soundManager.stopAll();
-			this.play()
+			this.play();
+			this.changeInfoSong();
+		}else{
+			this.i++;
 		}
 	}
 	this.next=function(){
 		this.i++;
 		if (this.i<this.arraySong.length && this.i>=0) {
 			soundManager.stopAll();
-			this.play()
+			this.play();
+			this.changeInfoSong();
+		}else{
+			this.i--;
 		}
+	}
+
+	this.changeInfoSong = function(){
+		var cancion = this.arraySong[this.i];
+		// Cambiar titulo
+		document.getElementById("player-title").innerHTML=cancion.title;
+		// Cambiar artista
+		document.getElementById("player-artist").innerHTML=cancion.artist;
+
+		// Cmabiar cover
+		var imgURL = cancion.cover;
+		document.getElementById("player").style.backgroundImage="url("+imgURL+")";
+	}
+
+	this.stop = function(){
+		soundManager.stopAll();
+		// Cambiar icono
+		document.getElementById("player-play").setAttribute("class","");
+		document.getElementById("player-play").setAttribute("class","glyphicon glyphicon-play");	
+	}
+
+	this.clear = function(){
+		this.arraySong =[];
 	}
 
 	// METODOS DE INICIO
