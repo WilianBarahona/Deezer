656e88c2c38f5561d5698fe0b20f54457471e41b
jaguilar992 <jaguilar992@gmail.com>
2017-06-24 10:01:50 -0600

    Plantillas Barra Flotante

--- js/panel.js
+++ js/panel.js
@@ -4,46 +4,64 @@ function panel(id){
 	this.visible=false;
 	this.event="";
 	this.hidePanel = function() {
-		this.panel.animate({
-			"margin-left":-500,
-			"display":"none"
-		},300);
-		$("#float-bar-overlay").css({"display": "none"});
+		this.visible=false;
+		$(this.panel).animate({"margin-left":-500,},250);
+		$(this.panel).fadeOut(250);
+		$("#float-bar-overlay").fadeOut();
 		$(".active-white").removeClass().addClass("active");
 		$("#txt-search").focusout();
-		this.visible=false;
 	}
 	this.showPanel= function(id){
-		this.event=id;
-		console.log("this.id: "+id)
-		var a = $("#sidebar").width();
-		this.panel.css({
-			"display":"block"
-		});
-		$(".active").removeClass().addClass("active-white")
-		this.panel.animate({
-			"margin-left":(a+30)
-		},300);
-		$("#float-bar-overlay").css({"display": "block"});
+		this.load(id);
+		var content=$("#float-bar-content");
 		this.visible=true;
+		this.event=id;
+		var a = $("#sidebar").width();
+		$(this.panel).show();
+		this.panel.animate({"margin-left":(a+30)},300);
+		$(".active").removeClass().addClass("active-white")
+		$("#float-bar-overlay").fadeIn();
 	}
 	this.toggle = function(id){
-		console.log(id);
-		console.log("(toggle)this.id: "+id)
 		if (this.visible) {
 			if (this.event==id) {
 				this.hidePanel();
 			}else{
-				showPanel(id);
+				this.event=id;
+				this.load(id);
 			}
 		}else{
 			this.showPanel(id);
 		}
 	}
-}
 
-function viewPlayList(){
-	console.log(lista);
-	// $("#list-bar").html("Hola")
-	listaBar.toggle();
+	this.load=function(id){
+		var content = $("#float-bar-content");
+		var path ="float-bar/";
+		switch(id){
+			case "txt-search":
+			content.load(path+"search.html");
+				break;
+			case "float-settings":
+				content.load(path+"settings.html");
+				break;
+			case "float-playlist": 	
+				content.load(path+"playlist.html");
+				break;
+			case "float-albums": 	
+				content.load(path+"albums.html");
+				break;
+			case "float-activity": 	
+				content.load(path+"activity.html");
+				break;
+			case "float-apps": 	
+				content.load(path+"apps.html");
+				break;
+			case "float-playlist-playing": 	
+				content.load(path+"cola.html");
+				break;
+			default:
+				console.log(id);
+		}
+	}
 }
