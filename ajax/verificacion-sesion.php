8fed40dd1cf39a22f071ab64ee9559f196ca0f1c
PaolaIzaguirre <eviizaguirre@yahoo.com>
2017-11-14 22:14:28 -0600

    boton desconectar
    funcionalidad de cerrar sesion

--- ajax/verificacion-sesion.php
+++ ajax/verificacion-sesion.php
@@ -3,12 +3,31 @@
 	include ("../class/class-conexion.php");
 	include ("../class/class-usuario.php");	
 	
-	$correo=$_POST["inputEmail"];
-	$password=$_POST["inputPassword"];
+			
+		$objConexion=new Conexion();
+	if (isset($_POST['accion'])) {
+		switch ($_POST["accion"]) {
+			case 'iniciar-sesion':
+				$correo=$_POST["inputEmail"];
+				$password=$_POST["inputPassword"];
 	
-	//$password = hash('sha512',$password); 		
-	$objConexion=new Conexion();
-	Usuario::verificarUsuario($objConexion,$correo,$password);		
+				//$password = hash('sha512',$password); 		
+				Usuario::verificarUsuario($objConexion,$correo,$password);
+				
+				break;
+			case 'cerrar-sesion':
+					session_start();
+					$_SESSION['status']=false;
+					$respuesta['loggedin'] = 0;
+					echo json_encode($respuesta);
+				break;
+			
+			default:
+				# code...
+				break;
+		}
+		# code...
+	}
 	$objConexion->cerrarConexion();
 
 ?>
