1214578a945238b8a830c6b172417391adf21ef0
farel24 <fabriciomurillo24@gmail.com>
2017-11-16 02:23:23 -0600

    Funcion actualizar

--- js/admin-album.js
+++ js/admin-album.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	listarArtistas();
 	listarAlbumes();
+	$("#btn-actualizar-album").hide();
 });
 
 function listarAlbumes(){
@@ -20,8 +21,8 @@ function listarAlbumes(){
 				'	<td>'+albumes.nombre_album +'</td>'+
 				'	<td>'+albumes.nombre_artista+'</td>'+
 				'	<td>'+albumes.anio + '</td>'+
-				'  	<td><button onclick="editarIdioma('+albumes.id_album+')" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>'+
-				'  	<button onclick="eliminarIdioma('+albumes.id_album+') class="btn btn-default btn-xs"><span class="glyphicon glyphicon-trash"></span></button></td>'+
+				'  	<td><button onclick="editarAlbum('+albumes.id_album+')" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>'+
+				'  	<button onclick="eliminarAlbum('+albumes.id_album+') class="btn btn-default btn-xs"><span class="glyphicon glyphicon-trash"></span></button></td>'+
 				'	</tr>';
 				$("#div-albumes #tbl-albumes tbody").append(fila);
 			}
@@ -112,6 +113,7 @@ $("#btn-guardar-album").click(function(){
 						content: 'Se insertó el registro'
 					});
 					$("#tbl-artistas tbody").html("");
+					limpiar();
 					listarAlbumes();
 				}
 				else
@@ -131,3 +133,96 @@ $("#btn-guardar-album").click(function(){
 		});
 	}
 });
+
+$("#btn-actualizar-album").click(function(){
+	var idAlbum = $("#txt-id-album").val();
+	var idArtista = $("#slc-artista").val();
+	var nombreAlbum = $("#txt-nombre-album").val();
+	var fecha = $("#txt-fecha").val();
+	var url = $("#txt-url-foto-album").val();
+	if(nombreAlbum!="")
+	{
+		$.ajax({
+			url: "../ajax/gestionar-album.php",
+			method:"POST",
+			data:{
+				"accion":"actualizar-registro",
+				"id_album":idAlbum,
+				"id_artista":idArtista,
+				"nombre_album":nombreAlbum,
+				"anio":fecha,
+				"album_cover_url":url
+			},
+			dataType: "JSON",
+			success: function(respuesta){
+				if(respuesta)
+				{
+					$.alert({
+						title: '¡Éxito!',
+						content: 'Se actualizo el registro'
+					});
+				}
+				else
+				{
+					$.alert({
+						title: '¡Ocurrió un problema!',
+						content: 'No se pudo actualizar el registro'
+					});
+				}
+			},
+			error: function(error){
+				console.log(error);
+			},
+			complete: function(){
+				$("#lista-carga-foto-album").hide();
+				$("#btn-actualizar-album").hide();
+				$("#btn-guardar-album").show();
+				limpiar();
+				$("#tbl-albumes tbody").html("");
+				listarAlbumes();
+			}
+		});
+	}
+});
+
+function editarAlbum(idAlbum)
+{
+	$("#lista-carga-foto-album").hide();
+	$("#btn-guardar-album").hide();
+	$("#btn-actualizar-album").show();
+	$.ajax({
+		url:'../ajax/gestionar-album.php',
+		type: 'POST',
+		data:{
+			"accion":"seleccionar",
+			"id_album":idAlbum
+		},
+		dataType: "JSON",
+		success:function(respuesta){
+			if(respuesta)
+			{
+				$.alert({
+					title: 'Editar registro',
+					content: 'Modifica los campos que lo necesiten'
+				});
+				$("#txt-url-foto-album").val(respuesta.album_cover_url);
+				$("#slc-artista").val(respuesta.id_artista);
+				$("#txt-nombre-album").val(respuesta.nombre_album);
+				$("#txt-fecha").val(respuesta.anio);
+				$("#txt-id-album").val(idAlbum);
+			}
+		},
+		error: function(error){
+			console.log(error);
+		},
+		complete: function(){
+			//TO-DO
+		}
+	});
+}
+
+function limpiar(){
+	$("#txt-nombre-album").val('');
+	$("#txt-fecha").val('');
+	$("#slc-artista").val('');
+}
